126935663bc1391230d8e9146748daff51461127
awbrunet <awbrunet@uvm.edu>
2014-10-13 14:23:11 -0400

    trying to set up STUPID BUTTON ARGH 8

--- assignment5.0/form.php
+++ assignment5.0/form.php
@@ -189,7 +189,7 @@ if (isset($_POST["btnSubmit"])) {
     // 
     // If its the first time coming to the form or there are errors we are going
     // to display the form.
-    if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) { // closing of if marked with: end body submit
+    if (isset($_POST["btnSubmit"]) /*AND empty($errorMsg))*/ { // closing of if marked with: end body submit
         print "<p>yay</p>";
 
         /*if (!$mailed) {
@@ -208,7 +208,8 @@ if (isset($_POST["btnSubmit"])) {
 
         print $message;*/
     } else {
-
+    }
+/*
 
         //####################################
         //
